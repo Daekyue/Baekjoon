82cdb1f2ee203c75b69d3ba8610982e91a6c173e
Daekyue <90326779+Daekyue@users.noreply.github.com>
2023-11-07 22:23:37 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -1,41 +1,46 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    while(true){
-        string s;
-        getline(cin, s);
-        if(s == ".") break;
-        stack<char> st;
-        bool check = true;
-        for (int i = 0; i< s.length(); i++)
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	while (1)
+	{
+		string s;
+		getline(cin, s);
+		if (s == ".")
+			break;
+		bool check = true;
+		stack <char> stck;
+		for (int i = 0; i< s.length(); i++)
 		{
 			char c = s[i];
-            if(c == '(' || c == '[')
-                st.push(c);
-            else if(c == ')')
-            {
-                if(st.empty() || st.top() != '(')
-                {
-                    check = false;
-                    break;
-                }
-                st.pop();
-            }
-            else if(c == ']')
-            {
-                if(st.empty() || st.top() != '[')
-                {
-                    check = false;
-                    break;
-                }
-                st.pop();
-            }
-        }
-        if(!st.empty()) check = false;
-        if(check) cout << "yes\n";
-        else cout << "no\n";
-    }
+			if (c == '(' || c == '[')
+				stck.push(c);
+			else if (c == ')')
+			{
+				if (stck.empty() || stck.top() != '(')
+				{
+					check = false;
+					break;
+				}
+				stck.pop();
+			}
+			else if (c == ']')
+			{
+				if (stck.empty() || stck.top() != '[')
+				{
+					check = false;
+					break;
+				}
+				stck.pop();
+			}
+		}
+		if (!stck.empty() || !check)
+			cout << "no\n";
+		else
+			cout << "yes\n";
+	}
 }
