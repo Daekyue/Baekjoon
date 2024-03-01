50119a38f2395438454b18275b75769225c92e94
Daekyue <90326779+Daekyue@users.noreply.github.com>
2024-03-01 23:52:59 +0900

    Update 최솟값 만들기.cpp

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
@@ -7,7 +7,7 @@ int solution(vector<int> A, vector<int> B)
 {
     int answer = 0;
     sort(A.begin(), A.end());
-    sort(B.begin(), B.end(),greater<>()); // sort함수에 greater<>() 사용시 내림차순 정렬 가능
+    sort(B.begin(), B.end(),greater<>()); // sort함수에 greater<>() 사용시 내림차순 정렬 가능 수정
     
     for (int i = 0; i < A.size(); i++)
     {
