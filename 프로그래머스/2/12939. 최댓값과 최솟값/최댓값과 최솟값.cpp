d696c20fc9162348e0d2e75a21c98e2409fc9ee6
Daekyue <90326779+Daekyue@users.noreply.github.com>
2024-03-04 20:11:54 +0900

    Update 최댓값과 최솟값.cpp

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 string solution(string s) {
     stringstream ss(s);
-    int min_num = 2147483647;
-    int max_num = -2147483648;
+    int min_num = 2147483647;        // int 범위 중 최댓값을 min 값으로 설정하여 어떤 값이 들어오더라도 최소값이 변경되게 설정
+    int max_num = -2147483648;        // int 범위 중 최소값을 max 값으로 설정하여 어떤 값이 들어오더라도 최대값이 변경되게 설정
     int num;
     
     while (ss >> num) {
