65ad6da76875f4943fd82003dcaaf51a4db4aed8
Daekyue <90326779+Daekyue@users.noreply.github.com>
2024-03-05 23:45:03 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 3.58 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(string message) {
+    int answer = 0;
+    answer = message.length() * 2;
+    return answer;
+}
