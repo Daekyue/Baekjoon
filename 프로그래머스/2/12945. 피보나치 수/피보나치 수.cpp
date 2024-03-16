6b879d28fe8b556c7f7d3df44153e0164dcfb3bd
Daekyue <90326779+Daekyue@users.noreply.github.com>
2024-03-16 11:02:19 +0900

    Update 피보나치 수.cpp

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -10,7 +10,7 @@ int solution(int n) {
     arr[1]=1;
     
     for(int i=2;i<=n;i++){
-        arr[i] = (arr[i-2]+arr[i-1])%1234567;
+        arr[i] = (arr[i-2]+arr[i-1])%1234567; // 해당 수는 적당히 큰 수와 소수라는 조건을 충족하여 1234567로 나눈 나머지를 구한다
     }
     
     answer = arr[n];
