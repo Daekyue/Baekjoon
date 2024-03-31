d0c213ac11ee648e9ef07cdde6a3131eaadac365
Daekyue <90326779+Daekyue@users.noreply.github.com>
2024-03-31 23:50:06 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.cpp
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 vector<int> solution(vector<string> strlist) {
-    vector<int> answer(strlist.size()); // answer 벡터 크기 설정
-
-    for (int i = 0; i < strlist.size(); i++) { // strlist.size()로 수정
+    vector<int> answer(strlist.size());
+    
+    for (int i = 0; i < strlist.size(); i++) {
         int count = 0;
-        while (strlist[i][count] != '\0') // '\n'을 '\0'로 수정
+        while (strlist[i][count] != '\0') 
             count++;
         answer[i] = count;
     }
