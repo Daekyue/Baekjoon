a890dedc566b6cd41e019389956a73d3dcf427f5
Daekyue <90326779+Daekyue@users.noreply.github.com>
2024-04-11 21:35:12 +0900

    [Silver III] Title: N과 M (3), Time: 488 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -1,18 +1,17 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int n, m;
 int arr[10];
 
-
 void func(int k)
 {
 	if (k == m)
 	{
-		for (int i = 0; i < m; i++)
-			cout << arr[i] << ' ';
-		cout << '\n';
+		for (int i = 0; i < k; i++)
+			cout << arr[i] << " ";
+		cout << "\n";
 		return;
 	}
 	for (int i = 1; i <= n; i++)
@@ -22,7 +21,7 @@ void func(int k)
 	}
 }
 
-int main(void)
+int main()
 {
 	cin >> n >> m;
 	func(0);
