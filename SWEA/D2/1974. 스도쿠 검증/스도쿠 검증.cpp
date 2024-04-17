69403485c0aee4cd113b3b55977e2d66c83a9c21
Daekyue <90326779+Daekyue@users.noreply.github.com>
2024-04-17 16:15:31 +0900

    [D2] Title: 스도쿠 검증, Time: 8 ms, Memory: 13,376 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.cpp
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.cpp
@@ -1,71 +1,93 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
 
 using namespace std;
 
 int arr[10][10];
 
-// 행의 중복 검사 함수
-bool checkRow() {
-    for (int i = 0; i < 9; ++i) {
-        vector<bool> visited(10, false); 
-        for (int j = 0; j < 9; ++j) {
-            if (visited[arr[i][j]]) 
-                return false;
-            visited[arr[i][j]] = true;
-        }
-    }
-    return true; 
-}
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	//freopen("input.txt", "r", stdin);
+	cin >> T;
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int answer = 1;
+		for (int i = 0; i < 9; i++)
+		{
+			int check = 0;
+			for (int j = 0; j < 9; j++)
+			{
+				cin >> arr[i][j];
+				check += arr[i][j];
+			}
+			if (check != 45)
+			{
+				answer = 0;
+			}
+		}
 
-// 열의 중복 검사 함수
-bool checkColumn() {
-    for (int j = 0; j < 9; ++j) {
-        vector<bool> visited(10, false); 
-        for (int i = 0; i < 9; ++i) {
-            if (visited[arr[i][j]]) 
-                return false;
-            visited[arr[i][j]] = true;
-        }
-    }
-    return true; 
-}
+		// 세로 검증
+		if (answer)
+		{
+			for (int i = 0; i < 9; i++)
+			{
+				int check = 0;
+				for (int j = 0; j < 9; j++)
+					check += arr[j][i];
+				if (check != 45)
+				{
+					answer = 0;
+					break;
+				}
+			}
+		}
 
-// 3x3 작은 정사각형 영역의 중복 검사 함수
-bool checkSquare() {
-    for (int row = 0; row < 9; row += 3) {
-        for (int col = 0; col < 9; col += 3) {
-            vector<bool> visited(10, false); 
-            for (int i = row; i < row + 3; ++i) {
-                for (int j = col; j < col + 3; ++j) {
-                    if (visited[arr[i][j]]) 
-                        return false;
-                    visited[arr[i][j]] = true;
-                }
-            }
-        }
-    }
-    return true; 
-}
+		// 세로 검증
+		if (answer)
+		{
+			for (int i = 0; i < 9; i++)
+			{
+				int check = 0;
+				for (int j = 0; j < 9; j++)
+					check += arr[j][i];
+				if (check != 45)
+				{
+					answer = 0;
+					break;
+				}
+			}
+		}
 
-int main(int argc, char** argv) {
-    int T;
-    cin >> T;
+		//3*3 검증
+		if (answer)
+		{
+			int dx[9]{ -1, -1, -1, 0,0,0, 1,1,1 };
+			int dy[9]{ -1,0,1,-1,0,1,-1,0,1};
 
-    for (int test_case = 1; test_case <= T; ++test_case) {
-        // 스도쿠 판 입력 받기
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                cin >> arr[i][j];
-            }
-        }
+			for (int i = 1; i < 9; i += 3)
+			{
+				for (int j = 1; j < 9; j += 3)
+				{
+					int check = 0;
+					for (int k = 0; k < 9; k++)
+					{
+						check += arr[i + dx[k]][j + dy[k]];
+					}
+					if (check != 45)
+					{
+						answer = 0;
+						break;
+					}
+				}
+				if (!answer)
+					break;
+			}
 
-        bool isValid = checkRow() && checkColumn() && checkSquare();
-        int answer = 1;
-        if (!isValid)
-            answer = 0;
-        cout << "#" << test_case << " " << answer << "\n";
-    }
-    return 0; // 정상 종료
+		}
+
+		cout << "#" << test_case << " " << answer << "\n";
+
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
