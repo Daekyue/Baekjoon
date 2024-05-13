8f885f68e4e728fac6175620c543ea808b19c284
Daekyue <90326779+Daekyue@users.noreply.github.com>
2024-05-13 17:03:21 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 8 ms, Memory: 13,380 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.cpp
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.cpp
@@ -1,28 +1,39 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	int test_case, t;
-	string fi, str;
+	int test_case;
+
 	for (test_case = 1; test_case <= 10; ++test_case)
 	{
-		cin >> t;
-		cin >> fi;
-		cin >> str;
-		int count = 0;
-		for (int i = 0; i < str.length(); i++)
+		int T;
+		cin >> T;
+		string find, target;
+		cin >> find;
+		cin >> target;
+
+		int cur = 0, count = 0, tmp = 0;
+
+		while (cur < target.size())
 		{
-			if (fi[0] == str[i])
+			if (target[cur] == find[tmp])
 			{
-				int j = 0;
-				while (fi[j] == str[i + j] && i+j != str.length())
-					j++;
-				if (j == fi.length())
-					count++;
+				while (target[cur + tmp] == find[tmp])
+				{
+					tmp++;
+					if (tmp == find.size())
+					{
+						count++;
+						break;
+					}
+				}
+				tmp = 0;
 			}
+			cur++;
 		}
+
 		cout << "#" << test_case << " " << count << "\n";
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
