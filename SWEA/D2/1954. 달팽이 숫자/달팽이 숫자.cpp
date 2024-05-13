f7eea7cf554a63b36c026e23bc7237e740c9f9b3
Daekyue <90326779+Daekyue@users.noreply.github.com>
2024-05-13 15:42:52 +0900

    [D2] Title: 달팽이 숫자, Time: 6 ms, Memory: 13,536 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.cpp
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.cpp
@@ -1,56 +1,49 @@
-#include<iostream>
 #include<bits/stdc++.h>
 
 using namespace std;
+
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0, 1, 0, -1};
+int arr[11][11];
 
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
-	int N;
-	
-	int board[10][10];
 	cin >> T;
 
-	int dx[4] = { 0, 1, 0, -1 };
-	int dy[4] = { 1, 0, -1, 0 };
-	
 	for (test_case = 1; test_case <= T; ++test_case)
-	{	
-		int num = 1;
-		int dist = 0;
-		int x = 0, y = 0;
-		cin >> N;
-		cout << "#" << test_case << "\n";
+	{
+		memset(arr, 0, sizeof(arr));
+		int n;
+		cin >> n;
+		int nx = 0, ny = 0;
+		int num = 0;
+		for (int i = 1; i <= n*n; i++)
+		{
+			arr[ny][nx] = i;
+			ny += dy[num%4];
+			nx += dx[num % 4];
 
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < N; j++)
-				board[i][j] = 0;
-		board[0][0] = num;
-		while (num != N * N)
-		{
-			num++;
-			x = x + dx[dist % 4];
-			y = y + dy[dist % 4];
-			if (x < 0 || y < 0 || x >= N || y >= N || board[x][y] != 0)
-			{if(num==6)
+			if (ny < 0 || ny >= n || nx < 0 || nx >= n || arr[ny][nx] != 0)
 			{
-				num = 6;
+				ny -= dy[num % 4];
+				nx -= dx[num % 4];
+
+				num++;
+
+				ny += dy[num % 4];
+				nx += dx[num % 4];
 			}
-				x = x - dx[dist % 4];
-				y = y - dy[dist % 4];
-				dist++;
-				num--;
-			}
-			board[x][y] = num;
 		}
-		for (int i = 0; i < N; i++)
+
+		cout << "#" << test_case << "\n";
+		for (int i = 0; i < n; i++)
 		{
-			for (int j = 0; j < N; j++)
-				cout << board[i][j] << " ";
+			for (int j = 0; j < n; j++)
+				cout << arr[i][j] << " ";
 			cout << "\n";
 		}
 	}
-
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
