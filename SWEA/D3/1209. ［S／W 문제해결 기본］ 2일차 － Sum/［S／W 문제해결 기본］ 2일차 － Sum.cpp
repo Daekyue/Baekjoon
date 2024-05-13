2695d877f1a13067ffc8f0283332b2991b974de0
Daekyue <90326779+Daekyue@users.noreply.github.com>
2024-05-13 21:38:03 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 46 ms, Memory: 13,544 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.cpp
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.cpp
@@ -1,49 +1,57 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
+
+int arr[100][100];
 
 int main(int argc, char** argv)
 {
 	int test_case;
-	
+
 	for (test_case = 1; test_case <= 10; ++test_case)
 	{
 		int T;
 		cin >> T;
-		int num[100][100];
-		int sum[203];
-		int max = 0;
-		memset(num, 0, sizeof(num));
-		memset(sum, 0, sizeof(sum));
-		for (int i = 0; i < 100; i++)	// 행 합
+		memset(arr, 0, sizeof(arr));
+		int maxSum = 0, crossSum = 0;
+
+		// 가로합 중 가장 큰 것
+		for (int i = 0; i < 100; i++)
 		{
+			int sum = 0;
 			for (int j = 0; j < 100; j++)
 			{
-				cin >> num[i][j];
-				sum[i] += num[i][j];
+				cin >> arr[i][j];
+				sum += arr[i][j];
 			}
+			maxSum = max(maxSum, sum);
 		}
 
-		for (int i = 0; i < 100; i++)	// 열 합
+		// 세로합 중 가장 큰 것
+		for (int i = 0; i < 100; i++)
 		{
+			int sum = 0;
 			for (int j = 0; j < 100; j++)
-				sum[i + 100] += num[j][i];
+			{
+				sum += arr[j][i];
+			}
+			maxSum = max(maxSum, sum);
 		}
-
-		for (int i = 0; i < 100; i++)	// 좌->우 대각선 합
-			sum[201] += num[i][i];
 
 		for (int i = 0; i < 100; i++)
-			sum[202] += num[i][100 - i];
-		
-
-		for (int i = 0; i < 203; i++)
 		{
-			if (max < sum[i])
-				max = sum[i];
+			crossSum += arr[i][i];
 		}
+		maxSum = max(maxSum, crossSum);
 
-		cout << "#" << test_case << " " << max << "\n";
+		crossSum = 0;
+		for (int i = 99; i >= 0; i--)
+		{
+			crossSum += arr[99-i][i];
+		}
+		maxSum = max(maxSum, crossSum);
+
+		cout << "#" << test_case << " " << maxSum <<"\n";
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
