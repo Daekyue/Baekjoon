baf049e56a818e7400c172c403e0761a25167b9a
Daekyue <90326779+Daekyue@users.noreply.github.com>
2024-05-13 22:00:23 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 7 ms, Memory: 13,544 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.cpp
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.cpp
@@ -2,45 +2,32 @@
 
 using namespace std;
 
-int num[100];
-int box[100];
+int arr[100];
+
 int main(int argc, char** argv)
 {
 	int test_case;
+
 	for (test_case = 1; test_case <= 10; ++test_case)
 	{
-		int dump;
-		cin >> dump;
+		memset(arr, 0, sizeof(arr));
+		int n;
+		cin >> n;
 
-		int min= 99 , max = 0;
-		memset(num, 0, sizeof(num));
-		memset(box, 0, sizeof(box));
 		for (int i = 0; i < 100; i++)
 		{
-			cin >> num[i];
-			box[num[i]]++;
-			if (min > num[i])
-				min = num[i];
-			if (max < num[i])
-				max = num[i];
+			cin >> arr[i];
 		}
-		for (int i = 0; i < dump; i++)
+		sort(arr, arr+100);
+
+		for (int i = 0; i < n; i++)
 		{
-			box[max]--;
-			box[max - 1]++;
-			box[min]--;
-			box[min+1]++;
-
-			if (box[max] == 0)
-				max--;
-
-			if (box[min] == 0)
-				min++;
-			
-			if (max - min <= 1)
-				break;
+			arr[0]++;
+			arr[99]--;
+			sort(arr, arr + 100);
 		}
-		cout << "#" << test_case << " " << max - min <<"\n";
+		
+		cout << "#" << test_case << " " << arr[99] - arr[0] <<"\n";
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
