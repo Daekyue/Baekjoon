394fb137d304b30650288e068f7df5f800fbf363
Daekyue <90326779+Daekyue@users.noreply.github.com>
2024-05-15 23:42:01 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 7 ms, Memory: 13,540 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.cpp
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.cpp
@@ -2,31 +2,27 @@
 
 using namespace std;
 
-int mem[50];
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
 	cin >> T;
+
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
-		string str;
-		cin >> str;
-		int cnt = 0;
-		int pre = 0;
-		for (int i = 0; i < str.length(); i++)
+		string s;
+		cin >> s;
+
+		int count = 0;
+		for (int i = 0; i < s.size(); i++)
 		{
-			mem[i] = str[i] - '0';
-			if (mem[i] != pre)
-			{
-				cnt++;
-				if (mem[i] == 0)
-					pre = 0;
-				else
-					pre = 1;
-			}
+			if (s[i] != s[i + 1])
+				count++;
 		}
-		cout << "#" << test_case << " " << cnt << "\n";
+		if (s[0] == '1')
+			count++;
+
+		cout << "#" << test_case << " " << count-1 << "\n";
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
