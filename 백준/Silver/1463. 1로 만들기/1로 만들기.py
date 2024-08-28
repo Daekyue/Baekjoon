ffc86695e57540f74b8abb759dde663e49109336
Daekyue <90326779+Daekyue@users.noreply.github.com>
2024-08-28 11:49:14 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 118248 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,13 @@
+d = [0] * 1000003
+
+num = int(input())
+d[1] = 0
+
+for i in range(2, num+1):
+    d[i] = d[i-1] + 1
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i//2] + 1)
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i//3] + 1)
+
+print(d[num])
