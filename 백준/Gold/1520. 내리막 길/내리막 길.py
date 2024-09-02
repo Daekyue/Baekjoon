064c8e06973eb697d553155a2ff29561996259d2
Daekyue <90326779+Daekyue@users.noreply.github.com>
2024-09-03 01:08:10 +0900

    [Gold III] Title: 내리막 길, Time: 180 ms, Memory: 120852 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,8 +1,6 @@
-import sys
-sys.setrecursionlimit(10000)
-
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
+
 
 def dfs(x, y):
     if x == n - 1 and y == m - 1:  # 목표 지점에 도달한 경우
@@ -10,14 +8,15 @@ def dfs(x, y):
     if dp[x][y] != -1:  # 이미 계산한 값이 있으면 그 값을 사용
         return dp[x][y]
 
-    dp[x][y] = 0  # 초기화
+    dp[x][y] = 0  # 처음 지나가는 길은 일단 0으로 초기화
     for d in range(4):
         nx = x + dx[d]
         ny = y + dy[d]
         if 0 <= nx < n and 0 <= ny < m and arr[x][y] > arr[nx][ny]:
-            dp[x][y] += dfs(nx, ny)
-    
+            dp[x][y] += dfs(nx, ny) # dfs가 돌아오면서 값을 채워넣으므로 print(0,0)을 진행
+
     return dp[x][y]
+
 
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
