79ac68801c1bfabc572f7f83ff753d93c685e4db
Daekyue <90326779+Daekyue@users.noreply.github.com>
2025-10-28 23:28:08 +0900

    [level 3] Title: 네트워크, Time: 0.57 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,17 @@
 # 한 줄안에 있는 애들 다 타고 들어가자
 # 들어갔을 때 visit
 
-def dfs(i, visit, computers, flag):
+def dfs(i, visit, computers):
     for j in range(len(computers[i])):
         if computers[i][j] and not visit[j]:
             visit[j] = 1
-            flag[0] = 1
-            dfs(j, visit, computers, flag)
+            dfs(j, visit, computers)
     
 def solution(n, computers):
     answer = 0
     visit = [0] * n
     for i in range(n):
-        flag = [0]
-        dfs(i, visit, computers, flag)
-        if flag[0]:
+        if not visit[i]:
+            dfs(i, visit, computers)
             answer += 1
     return answer
