c7204d9d5a46ded7815a524f566b7e4cdf8c0fc5
Daekyue <90326779+Daekyue@users.noreply.github.com>
2025-11-18 23:04:54 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 21.90 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,26 @@
+def solution(s):
+    answer = []
+    delete = 0
+    turn = 0
+    s = list(map(int,s))
+    
+    while len(s) != 1:
+        turn += 1
+        leng = len(s)
+        s = list(map(int,s))
+        sm = sum(s)
+        delete += leng - sm
+        
+        
+        # 2진 변환
+        s.clear()
+        
+        
+        while sm != 1:
+            s.append(sm % 2)
+            sm = sm // 2
+        s.append(1)
+    answer.append(turn)
+    answer.append(delete)
+    
+    return answer
