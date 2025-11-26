a58e6c86c993465c02bea1cec5fa966db109776a
Daekyue <90326779+Daekyue@users.noreply.github.com>
2025-11-26 22:57:20 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,13 @@
+def solution(n,a,b):
+    count = 0
+    while a != b:
+        if a % 2 != 0:
+            a += 1
+        if b % 2 != 0:
+            b += 1
+        
+        a //= 2
+        b //= 2
+        count += 1
+
+    return count
