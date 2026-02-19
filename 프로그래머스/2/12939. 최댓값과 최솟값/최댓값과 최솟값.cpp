4dd689339fcb731816c96e4389478c2f0586d64d
Daekyue <90326779+Daekyue@users.noreply.github.com>
2026-02-19 17:02:22 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.51 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -5,19 +5,19 @@
 using namespace std;
 
 string solution(string s) {
+    string answer = "";
     stringstream ss(s);
-    int min_num = 2147483647;        // int 범위 중 최댓값을 min 값으로 설정하여 어떤 값이 들어오더라도 최소값이 변경되게 설정
-    int max_num = -2147483648;        // int 범위 중 최소값을 max 값으로 설정하여 어떤 값이 들어오더라도 최대값이 변경되게 설정
     int num;
+    int maxn = -12345678;
+    int minn = 12345678;
     
-    while (ss >> num) {
-        if (num < min_num) {
-            min_num = num;
-        }
-        if (num > max_num) {
-            max_num = num;
-        }
+    while (ss >> num)
+    {
+        if (maxn <= num)
+            maxn = num;
+        if (minn >= num)
+            minn = num;
     }
-    
-    return to_string(min_num) + " " + to_string(max_num);
+    answer = to_string(minn) + " " + to_string(maxn);
+    return answer;
 }
